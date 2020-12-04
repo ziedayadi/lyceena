0da4b14c742c37fb9d64627060d153e1d7f655fd
Zied AYADI <zied.ayedi22@gmail.com>
2020-12-04 15:02:46 +0100

    Create M project

---
+++ backend/src/main/java/com/zka/lyceena/LyceenaApplication.java
@@ -0,0 +1,13 @@
+package com.zka.lyceena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyceenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyceenaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/zka/lyceena/LyceenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zka.lyceena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyceenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
