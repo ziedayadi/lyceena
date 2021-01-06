e2367e3eea626c6e04d4f2e02e71b5514e853656
Zied AYADI <zied.ayedi22@gmail.com>
2021-01-06 15:56:37 +0100

    [BE] [FE] : Class details #2

---
+++ frontend/src/app/components/class-details/class-details.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ClassesService } from 'src/app/services/classes.service';
+
+@Component({
+  selector: 'app-class-details',
+  templateUrl: './class-details.component.html',
+  styleUrls: ['./class-details.component.css']
+})
+export class ClassDetailsComponent implements OnInit {
+
+  class : any; 
+  constructor(private route: ActivatedRoute,
+    private classesService : ClassesService) { }
+
+  ngOnInit(): void {
+    const classId  = this.route.snapshot.paramMap.get('classId');
+    this.classesService.findOne(classId).subscribe(r=>this.class = r);
+  }
+}
