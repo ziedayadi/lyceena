319bb3cadb837336440652f42ebc5932d46fada1
Zied AYADI <zied.ayedi22@gmail.com>
2021-01-06 14:57:56 +0100

    [FE] : Fix crud-data-table actions columns width

--- frontend/src/app/services/classes.service.ts
+++ frontend/src/app/services/classes.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BASE_URL } from '../constants';
 
+
+const BACK_END_SERVICE_NAME = '/classes/';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,6 +14,15 @@ export class ClassesService {
    }
 
    public findAll() : Observable<any> {
-    return this.httpClient.get( BASE_URL +'/classes/');
+    return this.httpClient.get( BASE_URL + BACK_END_SERVICE_NAME);
+  }
+
+  
+  public save (parent) : Observable<any> {
+    return this.httpClient.post( BASE_URL + BACK_END_SERVICE_NAME, parent);
+  }
+
+  public remove(id : Number) : Observable<any> {
+    return this.httpClient.delete( BASE_URL +BACK_END_SERVICE_NAME+id);
   }
 }
