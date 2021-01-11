9df55ee00c3cfc460d8a4adb4d667cf197321a27
Zied AYADI <zied.ayedi22@gmail.com>
2021-01-11 13:07:30 +0100

    [BE] Init authentication server

---
+++ authentication-server/src/main/java/com/zka/lyceena/authserver/configuration/RegularJsonConfigProviderFactory.java
@@ -0,0 +1,6 @@
+package com.zka.lyceena.authserver.configuration;
+
+import org.keycloak.services.util.JsonConfigProviderFactory;
+
+public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
+}
