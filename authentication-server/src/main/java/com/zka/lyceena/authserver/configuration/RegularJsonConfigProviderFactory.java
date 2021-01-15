4977516da81dc4acc12d6704567dcc2c67871bab
Zied AYADI <zied.ayedi22@gmail.com>
2021-01-15 13:50:16 +0100

    [BE] Secure resource server : Authentication and Authorization

--- authentication-server/src/main/java/com/zka/lyceena/authserver/configuration/RegularJsonConfigProviderFactory.java
+++
@@ -1,6 +0,0 @@
-package com.zka.lyceena.authserver.configuration;
-
-import org.keycloak.services.util.JsonConfigProviderFactory;
-
-public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
-}
