e06060b880a2f5228f6f9d5e6ec5cd9f7a2c8702
Zied AYADI <zied.ayedi22@gmail.com>
2021-01-15 17:51:21 +0100

    [BE] Configure spring tests

---
+++ backend/src/test/java/com/zka/lyceena/test/MaterialRefTest.java
@@ -0,0 +1,59 @@
+package com.zka.lyceena.test;
+
+import com.zka.lyceena.controllers.MaterialReferenceController;
+import com.zka.lyceena.entities.material.Material;
+import com.zka.lyceena.services.MaterialRefService;
+import com.zka.lyceena.test.config.TestApplicationContextConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@ContextConfiguration(classes = TestApplicationContextConfig.class)
+@RunWith(SpringRunner.class)
+@WebMvcTest(MaterialReferenceController.class)
+public class MaterialRefTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private MaterialRefService service;
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN",})
+    public void givenMaterials_whenGetMaterials_thenReturnJsonArray()
+            throws Exception {
+
+        Material material = new Material();
+        material.setName("TEST MATERIAL");
+        material.setDescription("TEST MATERIAL DESCRIPTION");
+
+        List<Material> allMaterials = Collections.singletonList(material);
+
+        given(service.findAll()).willReturn(allMaterials);
+        System.out.println("*******");
+        mvc.perform(get("/ref/materials/").contentType(MediaType.APPLICATION_JSON_VALUE)
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].name", is(material.getName())));
+    }
+}
---
+++ backend/src/test/java/com/zka/lyceena/test/config/TestApplicationContextConfig.java
@@ -0,0 +1,63 @@
+package com.zka.lyceena.test.config;
+
+
+import com.zka.lyceena.security.UserDetailsProviderImpl;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@EnableJpaRepositories(basePackages = "com.zka.lyceena.dao")
+@EntityScan({"com.zka.lyceena.entities.*"})
+@Configuration
+@ComponentScan(basePackages = {
+        "com.zka.lyceena.controllers",
+        "com.zka.lyceena.services",
+}, basePackageClasses = {
+        UserDetailsProviderImpl.class
+})
+@EnableTransactionManagement
+public class TestApplicationContextConfig {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan("com.zka.lyceena.entities");
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        em.setJpaProperties(addotionalProperties());
+        return em;
+    }
+
+    private Properties addotionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto","create");
+        properties.setProperty("hibernate.dialect","org.hibernate.dialect.H2Dialect");
+        return properties;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:~/test");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
