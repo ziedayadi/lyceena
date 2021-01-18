746254376db4ce67e99dfca63a12c8be7744bc6a
Zied AYADI <zied.ayedi22@gmail.com>
2021-01-18 18:25:00 +0100

    lyceena-9 [BE] Menus Test - With @WithMockUser

--- backend/src/test/java/com/zka/lyceena/test/MenusTest.java
+++ backend/src/test/java/com/zka/lyceena/test/MenusTest.java
@@ -1,6 +1,5 @@
 package com.zka.lyceena.test;
 
-import com.zka.lyceena.constants.Roles;
 import com.zka.lyceena.controllers.MenusController;
 import com.zka.lyceena.test.config.TestApplicationContextConfig;
 import org.junit.Test;
@@ -9,11 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Collection;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -32,9 +37,18 @@ public class MenusTest {
 
 
     @Test
-    @WithMockUser(username = "admin_y", roles = {Roles.ADMIN,})
+    //@WithMockUser(username = "admin_y", roles = {Roles.ADMIN,})
+
     public void testAdminMenus() throws Exception {
-        mvc.perform(get(BASE_REF_URL).contentType(MediaType.APPLICATION_JSON_VALUE)
+        Jwt jwt = Jwt.withTokenValue("token")
+                .header("alg", "none")
+                .claim("sub", "user")
+                .build();
+        Collection<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("SCOPE_read");
+        JwtAuthenticationToken token = new JwtAuthenticationToken(jwt, authorities);
+
+
+        mvc.perform(get(BASE_REF_URL).contentType(MediaType.APPLICATION_JSON_VALUE).with(authentication(token))
         )
                 .andExpect(status().isOk());
     }
