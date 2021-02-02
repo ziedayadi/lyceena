66381b2d7c317ea82925411cf8573c86824b68a1
Zied AYADI <zied.ayedi22@gmail.com>
2021-02-02 16:41:32 +0100

    lyceena-53  Fix Bugs with CORS/DELETE - Fix Bug : HttpTrace screen, timestapm

--- backend/src/main/java/com/zka/lyceena/configuration/WebConfig.java
+++ backend/src/main/java/com/zka/lyceena/configuration/WebConfig.java
@@ -2,6 +2,7 @@ package com.zka.lyceena.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,11 +12,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Value( "${cors.allowed.origins}" )
-    private String signingKey;
+    private String allowedOrigin;
 
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins(signingKey);
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigin)
+                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name());
     }
 }
