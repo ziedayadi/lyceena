56c8b138ae31613a543ce47aebda74a361b51e68
Zied AYADI <zied.ayedi22@gmail.com>
2021-02-04 17:26:51 +0100

    lyceena-90 Add Version and school name to the navigation bar - Fix Build

---
+++ frontend/src/app/components/school-contact/school-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-school-contact',
+  templateUrl: './school-contact.component.html',
+  styleUrls: ['./school-contact.component.css']
+})
+export class SchoolContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
