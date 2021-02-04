eadd77e080ac72421dc7e458a712657064bb26ab
Zied AYADI <zied.ayedi22@gmail.com>
2021-02-04 17:51:23 +0100

    lyceena-94 [FE] Improve school contact component

--- frontend/src/app/components/school-contact/school-contact.component.ts
+++ frontend/src/app/components/school-contact/school-contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RefService } from 'src/app/services/ref.service';
 
 @Component({
   selector: 'app-school-contact',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./school-contact.component.css']
 })
 export class SchoolContactComponent implements OnInit {
+  globalRef: any;
 
-  constructor() { }
+  constructor(private refService : RefService) { }
 
   ngOnInit(): void {
+    this.refService.findGlobalRef().subscribe(r=>this.globalRef = r);
+  }
+
+  getSchoolInfo(code : String){
+    if(this.globalRef){
+      let param =  this.globalRef.filter(e=>e.code == code)[0]
+      if(param) return param.value 
+      else return 'NA'
+    }
+    else {
+      return 'NA'
+    }
   }
 
 }
