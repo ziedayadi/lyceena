ddd74f1eccdfdad6b88fa8a8823e8b25f4325ff2
Zied AYADI <zied.ayedi22@gmail.com>
2021-03-02 10:53:11 +0100

    lyceena-132 List sessions attendance for Administrator : FE Display sessions for admin

--- frontend/src/app/components/admin/admin-sessions-list/admin-sessions-list.component.ts
+++ frontend/src/app/components/admin/admin-sessions-list/admin-sessions-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { AttendanceService } from 'src/app/services/attendance.service';
 
 @Component({
@@ -11,13 +12,18 @@ export class AdminSessionsListComponent implements OnInit {
   constructor(public attendanceService: AttendanceService) { }
 
   sessions;
+  dataSource: MatTableDataSource<any>;
+  displayedColumns: string[] = ['date','class', 'material' ,'teacher', 'hour', 'class_room' , 'status' , 'action'];
 
   ngOnInit(): void {
     this.fetchSessions()
   }
 
   private fetchSessions() {
-    this.attendanceService.getAdminSessions().subscribe(r => this.sessions = r);
+    this.attendanceService.getAdminSessions().subscribe(r => {
+      this.sessions = r;
+      this.dataSource = new MatTableDataSource(this.sessions);
+    });
   }
 
 }
