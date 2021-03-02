634716cec1cefb80466300f5aa2760b5478ea828
Zied AYADI <zied.ayedi22@gmail.com>
2021-03-02 17:25:43 +0100

    lyceena-137 Session List for Student: FE Create component

---
+++ frontend/src/app/components/students/student-session-list/student-session-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-session-list',
+  templateUrl: './student-session-list.component.html',
+  styleUrls: ['./student-session-list.component.css']
+})
+export class StudentSessionListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
