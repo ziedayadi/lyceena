a3e535af7a0ffba84ed3233999c9cfa7f7890752
Zied AYADI <zied.ayedi22@gmail.com>
2021-03-02 11:35:04 +0100

    lyceena-132 List sessions attendance for Administrator : FE Add Sort to list

--- frontend/src/app/components/admin/admin-sessions-list/admin-sessions-list.component.ts
+++ frontend/src/app/components/admin/admin-sessions-list/admin-sessions-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { AttendanceService } from 'src/app/services/attendance.service';
 
@@ -14,8 +15,9 @@ export class AdminSessionsListComponent implements OnInit {
 
   sessions;
   dataSource: MatTableDataSource<any>;
-  displayedColumns: string[] = ['date','class', 'material' ,'teacher', 'hour', 'class_room' , 'status' , 'action'];
+  displayedColumns: string[] = ['date', 'class', 'material', 'teacher', 'hour', 'class_room', 'status', 'action'];
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) matSort: MatSort;
 
   ngOnInit(): void {
     this.fetchSessions()
@@ -26,7 +28,24 @@ export class AdminSessionsListComponent implements OnInit {
       this.sessions = r;
       this.dataSource = new MatTableDataSource(this.sessions);
       this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.matSort;
+      this.dataSource.sortingDataAccessor = this.sortingDataAccessor();
     });
+  }
+
+  sortingDataAccessor() {
+    return (item, property) => {
+      switch (property) {
+        case 'class': return item.classMaterialSession.clazz.levelName + item.classMaterialSession.clazz.name
+        case 'material': return item.classMaterialSession.material.name
+        case 'teacher': return item.classMaterialSession.teacher.firstName + item.classMaterialSession.teacher.lastName
+        case 'hour': return item.classMaterialSession.startHour.code
+        case 'class_room': return item.classMaterialSession.classRoom.name
+        case 'status': return this.attendanceService.getSessionAttendanceTraduction(item.status)
+        default:
+          return item[property];
+      }
+    };
   }
 
 }
