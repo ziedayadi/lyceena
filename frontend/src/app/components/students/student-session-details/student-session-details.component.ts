f5c46c522ae74480d6ab22bba1e0954aedcba167
Zied AYADI <zied.ayedi22@gmail.com>
2021-03-03 12:15:18 +0100

    lyceena-139 Open session by student FE : Add files section

--- frontend/src/app/components/students/student-session-details/student-session-details.component.ts
+++ frontend/src/app/components/students/student-session-details/student-session-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AttendanceService } from 'src/app/services/attendance.service';
+import { UploadFileService } from 'src/app/services/upload-file.service';
 
 @Component({
   selector: 'app-student-session-details',
@@ -10,16 +11,22 @@ import { AttendanceService } from 'src/app/services/attendance.service';
 export class StudentSessionDetailsComponent implements OnInit {
 
   sessionDetails;
+  files;
 
   constructor(private activatedRoute: ActivatedRoute,
-    public attendanceService: AttendanceService) { }
+    public attendanceService: AttendanceService,
+    private uploadFileService: UploadFileService
+  ) { }
 
   ngOnInit(): void {
     this.fetchSessionDetails();
   }
   private fetchSessionDetails() {
     const sessionId = this.activatedRoute.snapshot.paramMap.get('sessionId');
-    this.attendanceService.getSessionForStudentBySessionId(sessionId).subscribe(r => this.sessionDetails = r)
+    this.attendanceService.getSessionForStudentBySessionId(sessionId).subscribe(r => {
+      this.sessionDetails = r; 
+      this.fetchFiles(); 
+    })
   }
 
   getSessionPresence() {
@@ -28,5 +35,23 @@ export class StudentSessionDetailsComponent implements OnInit {
       code: this.sessionDetails.students[0].presence
     }
   }
+
+  fetchFiles() {
+    this.uploadFileService.getFilesBySessionId(this.sessionDetails.id).subscribe(r => {
+      this.files = r;
+    })
+  }
+
+  downloadFile(file){
+    console.log(file)
+    this.uploadFileService.donwnloadFile(file.id, file.type).subscribe(r=> {
+      console.log(r)
+      let data :any = r;
+      const blob = new Blob([data], { type: file.type });
+      const url= window.URL.createObjectURL(blob);
+      window.open(url);
+    })
+  }
+
 
 }
